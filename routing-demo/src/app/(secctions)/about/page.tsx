574209bcbd158d8e282e29c0235916e5034a5b03
Maria <maria.garcia@meltio3d.com>
2025-10-16 11:33:46 +0200

    Metadatos

--- routing-demo/src/app/(secctions)/about/page.tsx
+++ routing-demo/src/app/(secctions)/about/page.tsx
@@ -1,3 +1,10 @@
+//Exportamos un objeto de metadatos
+export const metadata = {
+    title:"About Codevolution",
+};
+
+
+
 export default function About() {
     return <h1>About me</h1>;
 }
--- routing-demo/src/app/(secctions)/products/[productId]/page.tsx
+++ routing-demo/src/app/(secctions)/products/[productId]/page.tsx
@@ -1,6 +1,24 @@
-export default async function ProductDetails({ params }:{
+//Metadatos de enrutamientos dinámicos
+import { Metadata } from "next";
+
+type Props = {
     params: Promise <{ productId: string}>;
-}) {
+};
+
+export const generateMetadata = async ({params} : Props): Promise<Metadata> =>{
+    const id = (await params).productId;
+    const title= await new Promise ((resolve) =>{
+        setTimeout(()=> {
+            resolve(`iPhone ${id}`);
+        })
+    })
+    return{
+        title: `Product ${title}`,
+
+    };
+}
+
+export default async function ProductDetails({ params }: Props) {
 
     const productId= (await params).productId
     return (
