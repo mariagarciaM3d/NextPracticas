e2ba2a11e7a9d2d3a35fd5d0605ea3921bb01e25
Maria <maria.garcia@meltio3d.com>
2025-10-16 16:34:03 +0200

    Controladores GET Y POST

--- routing-demo/src/app/(secctions)/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/(secctions)/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import {redirect} from "next/navigation";
+
 export default async function ProductReview({params}:
     {
         params: Promise <{productId: string; reviewId: string}>;
     }){
         const { productId, reviewId } = await params;
+        if (parseInt (reviewId) > 1000){
+            redirect("/products");
+        }
         return (
             <h1>
                 Review { reviewId } for product { productId }
