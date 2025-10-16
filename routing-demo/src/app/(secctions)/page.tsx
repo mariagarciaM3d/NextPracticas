31ad0f06fe13174b0727d267578a945c48dcc7ad
Maria <maria.garcia@meltio3d.com>
2025-10-16 13:17:28 +0200

    param, searchParams y use

--- routing-demo/src/app/(secctions)/page.tsx
+++ routing-demo/src/app/(secctions)/page.tsx
@@ -6,6 +6,13 @@ export default function Home(){
          <h1>Aprendiendo Next</h1>
          <Link href="/blog">Blog</Link>
          <Link href="/products">Products</Link>
+         {/* 
+         Parámetro de ruta dinámico:breaking-news-123
+         Parámetro de consulta: lang
+
+         */}
+         <Link href="/articles/breaking-news-123?lang=en">Read in english</Link>
+         <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
         </>
    
     );
