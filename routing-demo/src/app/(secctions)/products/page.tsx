dbba3ffaeaeef0a82f62b0218088aba0c531520f
Maria <maria.garcia@meltio3d.com>
2025-10-16 12:38:11 +0200

    link styles

--- routing-demo/src/app/(secctions)/products/page.tsx
+++ routing-demo/src/app/(secctions)/products/page.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 export default function ProductList(){
+    const productId=100;
     return (
         <>
+        <Link href="/">Home</Link>
         <h1>Product List</h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
+        <h2><Link href="/products/1">Product 1</Link></h2>
+        <h2><Link href="/products/2">Product 2</Link></h2>
+        <h2><Link href="/products/3">Product 3</Link></h2>
+        <h2><Link href={`/products/${productId}`} replace>Product {productId}</Link></h2>
+        {/*Propiedad replace si vas hacia atrás te lleva directamente al inicio */}
         </>
     );
 }
